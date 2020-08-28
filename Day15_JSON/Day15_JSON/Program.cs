d75ab3b3bf5420fd4ebb6bf91e7d0a7a6659a891
Felgiks <57964109+Felgiks@users.noreply.github.com>
2020-08-28 16:27:27 +0300

    Day 15 klases darbs
    Macamies JSON.

---
+++ Day15_JSON/Day15_JSON/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day15_JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
