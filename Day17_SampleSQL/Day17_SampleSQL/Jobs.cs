ad1a225eeba05cf690e5d6dd9e5e39871e692cc4
Felgiks <57964109+Felgiks@users.noreply.github.com>
2020-09-08 13:02:34 +0300

    "working from home"

---
+++ Day17_SampleSQL/Day17_SampleSQL/Jobs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day17_SampleSQL
+{
+    class Jobs
+    {
+        public String jobName;
+        public String jobDescr;
+
+        public Jobs(String jobName, String jobDescr)
+        {
+            this.jobName = jobName;
+            this.jobDescr = jobDescr;
+        }
+
+        public Jobs()
+        {
+            this.jobName = "";
+            this.jobDescr = "";
+        }
+    }
+}
---
+++ Day17_SampleSQL/Day17_SampleSQL/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day17_SampleSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connection con = new Connection();
+            con.Insert();
+            //con.Select();
+        }
+    }
+}
